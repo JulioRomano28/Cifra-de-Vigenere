1f9ad5a346e5f9915396b91107092005c4e704ec
Julio Romano <julio.cfr28@gmail.com>
2026-02-11 10:10:29 -0300

    Update do Código de Vigenere
    Em resumo, foi adicionado a função de Descriptografar uma frase a partir da frase criptografada + a chave

--- Cifra_de_Vigenere.py
+++ Cifra_de_Vigenere.py
@@ -1,47 +1,82 @@
-def repetir(frase, chave): #Looping que cria a frase com chave
+def repetir(frase, chave): #Função Responsavel por repetir a chave pello tamanho da frase
     chave_rep = ""
     i = 0
     for letra in frase:
         if letra.isalpha():
             chave_rep += chave[i % len(chave)]
-            i += 1 
-        else: chave_rep += " "
+            i += 1
+        else:
+            chave_rep += " "
     return chave_rep
-print("Bem Vindo a Cifra de Vigenere!\nUma forma de criptografia bem interessante!!!\n")
+
+
+def vigenere_criptografar(frase, chave): #Função Responsavel por criptografar
+    resul = ""
+
+    for cripA, cripB in zip(frase, chave):
+        if cripA.isalpha():
+            cripA = cripA.lower()
+            cripB = cripB.lower()
+
+            x = ord(cripA) - 97
+            y = ord(cripB) - 97
+
+            result = (x + y) % 26
+            resul += chr(result + 97)
+        else:
+            resul += cripA
+
+    return resul
+
+
+def vigenere_descriptografar(frase, chave): #Função Responsavel por descriptografar
+    resul = ""
+
+    for cripA, cripB in zip(frase, chave):
+        if cripA.isalpha():
+            cripA = cripA.lower()
+            cripB = cripB.lower()
+
+            x = ord(cripA) - 97
+            y = ord(cripB) - 97
+
+            result = (x - y) % 26
+            resul += chr(result + 97)
+        else:
+            resul += cripA
+
+    return resul
+
+# FIM DAS FUNÇÕES
+
+
+# PROGRAMA PRINCIPAL
+
+
+print("Bem Vindo a Cifra de Vigenere!\n")
 antes = input("Quer saber como a cifra de Vigenere Funciona? Digite 'S', caso contrário digite qualquer coisa: ")
 
 if antes == "s" or antes == "S":
-    print("\nCerto irei citar um breve resumo!\nA cifra de Vigenere pega uma frase/palavra e para criptografar pede uma chave, essa chave serve para ser repetida pelo tamanho da frase/palavra.\n Exemplo -\nFrase: Batata quente\nChave: fone.\nA nova frase fica: fonefo nefone\n\nApartir disso é analisado em uma tabela de vigenere a 1ª letra da sua frase com a 1ª letra da frase nova, e isso segue pela frase inteira até termos a frase criptografada!\nEspero ter ajudado, agora vamos por em prática?\n")
+    print("\nCerto irei citar um breve resumo!\nA cifra de Vigenere pega uma frase/palavra e para criptografar pede uma chave, essa chave serve para ser repetida pelo tamanho da frase/palavra.\n Exemplo -\nFrase: Batata quente\nChave: fone.")
+    print("A nova frase fica: fonefo nefone\n\nApartir disso é analisado em uma tabela de vigenere a 1ª letra da sua frase com a 1ª letra da frase nova, e isso segue pela frase inteira até termos a frase criptografada!\nEspero ter ajudado, agora vamos por em prática?\n")
 
-frase = input("\nDigite a Frase que deseja criptografar: ")
-chave = input("Digite sua Chave (palavra que sera repetida): ")
+
+
+opcao = input("\nDigite 1 para Criptografar ou 2 para Descriptografar: ")
+
+frase = input("\nDigite a frase: ")
+chave = input("Digite a chave: ")
 
 chave_final = repetir(frase, chave)
 
-print("Frase: ", frase)
-print("Chave: ", chave_final)
-resul = " "
+if opcao == "1":
+    resultado = vigenere_criptografar(frase, chave_final)
+    print("\nTexto Criptografado:", resultado)
 
-for cripA, cripB in zip(frase,chave_final): #Looping que criptografa as frases
+elif opcao == "2":
+    resultado = vigenere_descriptografar(frase, chave_final)
+    print("\nTexto Descriptografado:", resultado)
 
-    if cripA.isalpha():
-        if cripA.isupper():
-            cripA = chr(ord(cripA) + 32)
-        
-        if cripB.isupper():
-            cripB = chr(ord(cripB) + 32)
-        #essa parte faz uma letra maiuscula vira minuscula
-    else: resul += " "
-    x = ord(cripA) - 96
-    y = ord(cripB) - 96
-    
-    result = (x + (y - 1)) % 26 #Essa é a lógica que criptografa as frases
-    resultL = chr(result + 96)
-    resul += resultL
-
-  
-print(f"\nA sua frase Criptograda fica:{resul}")
-
-
-
+else:
+    print("Opção inválida.")
 
